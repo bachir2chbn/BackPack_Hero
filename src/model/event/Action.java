65caac3b1d40b420cb745fbb70bd89bda8f94494
CHIBANE Boualem <bchibane03@gmail.com>
2025-12-07 23:12:26 +0100

    Add Javadoc comments to Action enum
    Added documentation for Action enum.

--- src/model/event/Action.java
+++ src/model/event/Action.java
@@ -1,5 +1,12 @@
 package model.event;
 
+/**
+ * Actions simples qu'un ennemi peut choisir pendant son tour.
+ *
+ * @author bachir2chbn
+ * @author Mohammed442a
+ * @version 1.0
+ */
 public enum Action {
 	ATTACK, DEFEND
 }

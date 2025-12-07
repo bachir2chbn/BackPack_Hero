31c690fe6276fcb76657800ffe7ef71c45960134
CHIBANE Boualem <bchibane03@gmail.com>
2025-12-07 23:05:12 +0100

    Add documentation for Rarity enum
    Added Javadoc comments for the Rarity enum.

--- src/model/items/Rarity.java
+++ src/model/items/Rarity.java
@@ -1,5 +1,12 @@
 package model.items;
 
+/**
+ * Rareté possible d'un objet.
+ *
+ * @author bachir2chbn
+ * @author Mohammed442a
+ * @version 1.0
+ */
 public enum Rarity {
 	commun, uncommun, rare, legendary
 

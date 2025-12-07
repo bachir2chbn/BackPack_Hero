e60cdf35f1bba379eee2c96a5ee788a594cc5b12
CHIBANE Boualem <bchibane03@gmail.com>
2025-12-07 23:05:41 +0100

    Add JavaDoc comments to ArmorName enum
    Added documentation for ArmorName enum.

--- src/model/items/ArmorName.java
+++ src/model/items/ArmorName.java
@@ -1,5 +1,13 @@
 package model.items;
 
+/**
+ * Noms disponibles pour les armures ; utilisés pour l'affichage et la logique
+ * d'initialisation.
+ *
+ * @author bachir2chbn
+ * @author Mohammed442a
+ * @version 1.0
+ */
 public enum ArmorName {
 	RoughBuckler, KnightsShield, ironHelmet, spikedHelmet, bronzeBreastplate, tunic, leftGlove, rightGlove, leatherBoots,
 	steelBoots, brick, brickwall

3fa8e44c9f9a50e928439791fefbbbe2e8a0f661
CHIBANE Boualem <bchibane03@gmail.com>
2025-12-07 23:11:22 +0100

    Add Javadoc comments to Point class
    Added Javadoc comments to the Point class for better documentation.

--- src/model/dungeon/Point.java
+++ src/model/dungeon/Point.java
@@ -1,5 +1,12 @@
 package model.dungeon;
 
+/**
+ * Représente une position dans la grille du donjon.
+ *
+ * @author bachir2chbn
+ * @author Mohammed442a
+ * @version 1.0
+ */
 public record Point(int x, int y) {
 	public boolean isAdjacent(Point other) {
     return Math.abs(x - other.x) + Math.abs(y - other.y) == 1;

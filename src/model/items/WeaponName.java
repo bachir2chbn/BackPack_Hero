0eb036935bb8a3dcd40034c1678711768d236187
CHIBANE Boualem <bchibane03@gmail.com>
2025-12-07 23:06:02 +0100

    Add Javadoc comments to WeaponName enum
    Added Javadoc comments for the WeaponName enum.

--- src/model/items/WeaponName.java
+++ src/model/items/WeaponName.java
@@ -1,5 +1,12 @@
 package model.items;
 
+/**
+ * Noms possibles pour les armes.
+ *
+ * @author bachir2chbn
+ * @author Mohammed442a
+ * @version 1.0
+ */
 public enum WeaponName {
 	woodenSword, dagger, machete, chipppedSword, doru, bowblade,
 
